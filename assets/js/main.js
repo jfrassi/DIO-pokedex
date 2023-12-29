30a6ce039317147ed7e3aa414520f9ce7efd49a4
Joao Gouvea <joaofrassi.dev@gmail.com>
2023-12-29 16:59:13 -0300

    consumo de api, e manipulação de html

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+
+
+function pokemonToLi (pokemon) {
+    return ` 
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>      
+    </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += pokemonToLi(pokemon)
+        }
+    })
+    .catch((error) => console.log(error))
